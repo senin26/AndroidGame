28636b3cf7e03e5a0af01ac88c13e7290601556d
senin26 <sergey.ionin.7@gmail.com>
2019-01-13 20:16:32 +0300

    New background with appropriate size was added

--- SpaceWar/core/src/com/mygdx/game/MyGdxGame.java
+++ SpaceWar/core/src/com/mygdx/game/MyGdxGame.java
@@ -10,27 +10,22 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 0, 0, 150, 150);
+		img = new Texture("background.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0.26f, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.draw(region, 300, 300);
-		batch.setColor(0.5f, 0.1f, 0.3f, 0.5f);
-		batch.setColor(0.7f, 0.3f, 0.3f, 0.2f);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
